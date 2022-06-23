124dc0c49b95610fcbae3624ec46505796bc634c
angelDariaux <angeldariaux@gmail.com>
2022-06-22 21:33:05 -0600

    ahora si ya funcionan las pruebas en python3 usando ubuntu, creando un archivo .py from scratch para probar nueva sintaxis de python

--- seleniumPythonCourse/hello_world.py
+++ seleniumPythonCourse/hello_world.py
@@ -5,7 +5,7 @@ from selenium import webdriver
 class HelloWord(unittest.TestCase):
 
   @classmethod
-  def setUpClass(cls) -> None:
+  def setUpClass(cls):
     cls.driver = webdriver.Chrome(executable_path = './chromedriver') # traemos el driver para hacer las pruebas con Chrome
     driver = cls.driver
     driver.implicitly_wait(10) # hace que el programa espere 10 seg entre cada accion.
@@ -18,7 +18,7 @@ class HelloWord(unittest.TestCase):
     self.driver.get('https://www.wikipedia.org')
 
   @classmethod
-  def tearDown(cls) -> None:
+  def tearDown(cls):
     cls.driver.quit() # Esto cierra la ventana para evitar el uso de recursos. 
 
 if __name__ == "__name__":
