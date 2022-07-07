4eeb97be8b7ce22891bfcdb9acec499dd29e5d21
angelDariaux <angeldariaux@gmail.com>
2022-07-07 16:25:36 -0600

    modificado assetions2 y seartest2

--- seleniumPythonCourse/assertions2.py
+++ seleniumPythonCourse/assertions2.py
@@ -13,12 +13,19 @@ class AssertionsTest(unittest.TestCase):
       driver.maximize_window()
       driver.get('http://demo-store.seleniumacademy.com/')
 
+  def test_search_field(self):
+    self.assertTrue(self.is_element_present(By.NAME, 'q'))
+
+  def test_language_option(self):
+    self.assertTrue(self.is_element_present(By.ID, 'select-language'))
+
+  def tearDown(self):
+    self.driver.quit()
+
+
   def is_element_present(self, how, what):
     try:
       self.driver.find_element(by= how, value= what)
     except NoSuchElementException as variable:
       return False
     return True
-
-  def tearDown(self):
-    self.driver.quit()
--- seleniumPythonCourse/searchTest2.py
+++ seleniumPythonCourse/searchTest2.py
@@ -1,3 +1,4 @@
+from itertools import product
 import unittest # ? Esto nos da un reporte de las pruebas en la consola.
 from pyunitreport import HTMLTestRunner
 from selenium import webdriver
@@ -6,7 +7,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By 
 from selenium.webdriver.chrome.service import Service
 
-class Search(unittest.TestCase):
+class SearchTest(unittest.TestCase):
 
     def setUp(self):
 
@@ -19,31 +20,26 @@ class Search(unittest.TestCase):
         driver.maximize_window()
         driver.implicitly_wait(5) # ? hace que espere 5 segundos
 
-    def test_search_text_field(self):
-      search_field = self.driver.find_element(By.ID, "search") # ID "search" viene de la pagina web que queremos testear.
+    def test_search_tee(self):
+      driver = self.driver
+      search_field = driver.find_element_by_class_name('q')
+      search_field.clear()
 
-    def test_search_text_field_by_name(self):
-      search_field = self.driver.find_element(By.NAME, "q") # 'q' es el nombre de la etiqueta que estamos testeando
+      search_field.send_keys('tee')
+      search_field.submit()
 
-    def test_search_text_field_class_name(self):
-      search_field = self.driver.find_element(By.CLASS_NAME, "input-text") 
+    def test_search_salt_shaker(self):
+      driver = self.driver
+      search_field = driver.find_element(By.NAME, 'q')
 
-    def test_search_button_enabled(self):
-      button = self.driver.find_element(By.CLASS_NAME, "button")
+      search_field.send_keys('salt shaker')
+      search_field.submit()
 
-    def test_count_of_promo_images(self): # ? contara el numero de fotos en el banner de promo
-      banner_list = self.driver.find_element(By.CLASS_NAME, "promos")
-      banners = banner_list.find_elements(By.TAG_NAME, 'img') # ? cuenta los tags "img" dentro del banner "promos'"
-      self.assertEqual(3, len(banners)) # ? verifica si son 3 las imagenes del banner comparado con el largo del banner
-
-    def test_vip_promo(self):
-        vip_promo = self.driver.find_element(By.XPATH,"//*[@id='top']/body/div/div[2]/div[2]/div/div/div[2]/div/ul/li[4]/a/img")
-
-    def test_shopping_cart (self):
-      shopping_cart_icon = self.driver.find_element(By.CSS_SELECTOR, "div.header-minicart span.icon")
+      products = driver.find_elements(By.XPATH, '//*[@id="top"]/body/div/div[2]/div[2]/div/div[2]/div[2]/div[3]/ul/li/a')
+      self.assertEqual(1, len(products))
 
     def tearDown(self):
-        self.driver.quit() # cierra la ventana para evitar el consumo de recursos
+      self.driver.quit()
 
 if __name__ == '__main__':
     unittest.main(verbosity = 2) # verbosity = 2 indica el nivel de detalle que la consola nos regresara.
