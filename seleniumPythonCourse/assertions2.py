52c796ad726e21018ecaf623a23ec0284287f94b
angelDariaux <angeldariaux@gmail.com>
2022-07-07 16:08:09 -0600

    usando el NoSuchElementException

--- seleniumPythonCourse/assertions2.py
+++ seleniumPythonCourse/assertions2.py
@@ -1,6 +1,7 @@
 import unittest
 from selenium import webdriver
 from selenium.common.exceptions import NoSuchElementException # Esto nos permitira hacer exceptiones para nuestras assertions.
+from selenium.webdriver.common.by import By
 
 
 class AssertionsTest(unittest.TestCase):
@@ -11,6 +12,13 @@ class AssertionsTest(unittest.TestCase):
       driver.implicitly_wait(30) # hace que espere 30 segundos
       driver.maximize_window()
       driver.get('http://demo-store.seleniumacademy.com/')
+
+  def is_element_present(self, how, what):
+    try:
+      self.driver.find_element(by= how, value= what)
+    except NoSuchElementException as variable:
+      return False
+    return True
 
   def tearDown(self):
     self.driver.quit()
