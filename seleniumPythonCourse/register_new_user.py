09f3d30ccd33d0330045c68f1eba3050d105b2f3
angelDariaux <angeldariaux@gmail.com>
2022-07-07 22:58:44 -0600

    creando automatizacion para registrar a un nuevo usuario

---
+++ seleniumPythonCourse/register_new_user.py
@@ -0,0 +1,28 @@
+from lib2to3.pgen2 import driver
+import unittest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+
+class RegisterNewUser(unittest.TestCase):
+
+  def setUp(self):
+    self.driver = webdriver.Chrome(executable_path= './chromedriver')
+    driver = self.driver
+    driver.implicitly_wait(30)
+    driver.maximize_window()
+    driver.get('http://demo-store.seleniumacademy.com')
+
+  def test_new_user(self):
+    driver = self.driver
+    driver.find_element(By.XPATH, '/html/body/div/div[2]/header/div/div[2]/div/a/span[2]').click()  # ? click(), para abrir el log in de la pagina como si fuera un click humano
+    driver.find_element(By.LINK_TEXT, 'Log In').click() # ? By.LINK_TEXT hace que se seleccione el texto indicado como link, en este caso el link es Log In
+
+    create_account_button = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div/div/div[2]/form/div/div[1]/div[2]/a/span/span')
+    self.assertTrue(create_account_button.is_displayed() and create_account_button.is_enabled()) # todo: .is_displayed() muestra si el boton esta visible y .is_enabled() muestra si esta habilitado, si esto es verdadero el siguiente paso es darle click
+    create_account_button.click()
+    
+
+  def tearDown(self):
+    self.driver.implicitly_wait(3)
+    self.driver.close()
