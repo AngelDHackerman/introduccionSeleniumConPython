ca69761fe3efe8f572769e460bf43010ad7c1c74
angelDariaux <angeldariaux@gmail.com>
2022-07-07 23:11:42 -0600

    escribiendo los campos que vamos a testear

--- seleniumPythonCourse/register_new_user.py
+++ seleniumPythonCourse/register_new_user.py
@@ -21,8 +21,21 @@ class RegisterNewUser(unittest.TestCase):
     create_account_button = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div/div/div[2]/form/div/div[1]/div[2]/a/span/span')
     self.assertTrue(create_account_button.is_displayed() and create_account_button.is_enabled()) # todo: .is_displayed() muestra si el boton esta visible y .is_enabled() muestra si esta habilitado, si esto es verdadero el siguiente paso es darle click
     create_account_button.click()
-    
+
+    self.assertEqual('Create New Customer Account', driver.title) # ? Esto verifica si el titulo de la ventana es igual al titulo de la ventana de nuestro driver. 
+
+    first_name = driver.find_element(By.ID, 'firstname')
+    middle_name = driver.find_element(By.ID, 'middlename') 
+    last_name = driver.find_element(By.ID, 'lastname')
+    email_address = driver.find_element(By.ID, 'email_address') 
+    news_letter_subscription = driver.find_element(By.ID, 'is_subscribed')
+    password = driver.find_element(By.ID, 'password')
+    confirm_password = driver.find_element(By.ID, 'confirmation')
+    submit_button = driver.find_element(By.XPATH, '/html/body/div/div[2]/div[2]/div/div/div[2]/form/div[2]/button/span/span')
 
   def tearDown(self):
     self.driver.implicitly_wait(3)
     self.driver.close()
+
+if __name__ == '__main__':
+  unittest.main(verbosity= 2)
