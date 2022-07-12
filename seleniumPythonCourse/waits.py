15a311326c1bf78d66a91ac8f454838b89e294d5
angelDariaux <angeldariaux@gmail.com>
2022-07-11 22:21:51 -0600

    corregidos los errores, la prueba ya funciona

--- seleniumPythonCourse/waits.py
+++ seleniumPythonCourse/waits.py
@@ -10,7 +10,7 @@ class ExplicitWaitTest(unittest.TestCase):
     self.driver.get('http://demo-store.seleniumacademy.com')
 
   def test_account_link(self):
-    WebDriverWait(self.driver, 10).until(lambda s: s.find_element(By.ID, 'select-language').get_attribute('lenght') == '3')
+    WebDriverWait(self.driver, 10).until(lambda s: s.find_element(By.ID, 'select-language').get_attribute('length') == '3')
 
     account = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.LINK_TEXT, 'ACCOUNT'))) 
     account.click()
@@ -18,10 +18,10 @@ class ExplicitWaitTest(unittest.TestCase):
   def test_create_new_customer(self):
     self.driver.find_element(By.LINK_TEXT, 'ACCOUNT').click()
 
-    my_account = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(By.LINK_TEXT, 'My account'))
+    my_account = WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located((By.LINK_TEXT, 'My Account')))
     my_account.click()
 
-    create_account_button = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable(By.LINK_TEXT, 'CREATE AN ACCOUNT'))
+    create_account_button = WebDriverWait(self.driver, 20).until(EC.element_to_be_clickable((By.LINK_TEXT, 'CREATE AN ACCOUNT')))
     create_account_button.click()
 
     WebDriverWait(self.driver, 10).until(EC.title_contains('Create New Customer Account'))
