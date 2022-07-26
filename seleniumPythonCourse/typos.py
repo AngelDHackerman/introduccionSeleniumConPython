ae1b621714f1f30c9a14d3535a85b41d7eedbcc9
angelDariaux <angeldariaux@gmail.com>
2022-07-25 22:42:49 -0600

    codigo base para verificar typos en la pagina

--- seleniumPythonCourse/typos.py
+++ seleniumPythonCourse/typos.py
@@ -1,3 +1,4 @@
+from cgitb import text
 from lib2to3.pgen2 import driver
 import unittest
 from selenium import webdriver
@@ -14,7 +15,28 @@ class Typos(unittest.TestCase):
     driver.find_element(By.LINK_TEXT, 'Typos').click()
 
   def test_find_typos(self):
-    pass
+    driver = self.driver
+
+    paragraph_to_check = driver.find_element(By.CSS_SELECTOR, '#content > div > p:nth-child(3)')
+    text_to_check = paragraph_to_check.text
+    print(text_to_check)
+
+    tries = 1
+    found = False
+    correct_text = "Sometimes you'll see a typo, other times you won't."
+
+    while text_to_check != correct_text:
+      driver.refresh()
+
+    while not found: 
+      if text_to_check == correct_text:
+        tries += 1
+        # driver.refresh()
+        found = True
+
+    self.assertEqual(found, True)
+
+    print(f'It took {tries} tries to find the typo')
 
   def tearDown(self):
     self.driver.close()
